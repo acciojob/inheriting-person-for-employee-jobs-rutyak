8e0fa7faef7ca62927dfbaf46c40830e0e5a29c8
rutyak <132219103+rutyak@users.noreply.github.com>
2023-08-14 01:14:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
-
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
-}
-
-// this establish prototype inheritance
-Employee.prototype = Object.create(Person.prototype); // this will create new object that inherited from person prototype
-Employee.prototype.constructor = Employee; //  pointing to  emplyee constructor function
-
-Person.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
-
-const p = Person("Adarsh", 22);
-// const e = Employee("Swapnil", 23, "Businessman");
-
-p.greet();
-p.jobGreet();
-
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+    this.name = name;
+    this.age = age;
+  }
+  
+  Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  };
+  
+  function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+  }
+  
+  // Establish prototype inheritance
+  Employee.prototype = Object.create(Person.prototype);
+  Employee.prototype.constructor = Employee;
+  
+  Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  };
+  
+  // Creating instances using constructors and the 'new' keyword
+  const p = new Person("Adarsh", 22);
+  const e = new Employee("swapnil", 23, "Photographer");
+  
+  p.greet();
+  e.jobGreet();
+  
+  // Do not change code below this line
+  window.Person = Person;
+  window.Employee = Employee;
+  
