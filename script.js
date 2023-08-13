21af5b5c8d250d9c540105de8e5daafa9735c9da
rutyak <132219103+rutyak@users.noreply.github.com>
2023-08-14 00:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ Person.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
+Person(Adarsh, 22);
+Employee("Photographer");
 
 
 // Do not change code below this line
